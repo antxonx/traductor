16db5b4ea18cbc41d7cbaf3da0c167a186f18734
alejandro-antonio <alejandro@pcpartners.com.mx>
2021-02-26 09:54:17 -0600

    Recorrido de automata

--- lexico/Analizer.cs
+++ lexico/Analizer.cs
@@ -8,9 +8,9 @@ namespace Lexico
     {
         public static readonly string DIGIT_CHARSET = "0123456789";
 
-        public static readonly string LETTER_CHARSET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static readonly string TEXT_CHARSET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-        public static readonly char EMPTY_CHAR = (char)0;
+        public static readonly char NULL_CHAR = (char)0;
 
         public static readonly char DECIMAL_POINT_CHAR = '.';
 
@@ -54,7 +54,7 @@ namespace Lexico
             }
             else
             {
-                return Analizer.EMPTY_CHAR;
+                return Analizer.NULL_CHAR;
             }
         }
 
@@ -75,6 +75,72 @@ namespace Lexico
 
         public LexType GetNextState(in StatePos state, in char entry)
         {
+            switch (state)
+            {
+                case StatePos.Q0:
+                    if (Analizer.DIGIT_CHARSET.Contains(entry))
+                    {
+                        return this.GetNextState(StatePos.Q1, this.NextChar());
+                    }
+                    else if (Analizer.TEXT_CHARSET.Contains(entry))
+                    {
+                        return this.GetNextState(StatePos.Q4, this.NextChar());
+                    }
+                    break;
+                case StatePos.Q1:
+                    if (entry.Equals(Analizer.NULL_CHAR))
+                    {
+                        return LexType.INTEGER;
+                    }
+                    else
+                    {
+                        if (Analizer.DIGIT_CHARSET.Contains(entry))
+                        {
+                            return this.GetNextState(StatePos.Q1, this.NextChar());
+                        }
+                        else if (entry.Equals('.'))
+                        {
+                            return this.GetNextState(StatePos.Q2, this.NextChar());
+                        }
+                    }
+                    break;
+                case StatePos.Q2:
+                    if (Analizer.DIGIT_CHARSET.Contains(entry))
+                    {
+                        return this.GetNextState(StatePos.Q3, this.NextChar());
+                    }
+                    break;
+                case StatePos.Q3:
+                    if (entry.Equals(Analizer.NULL_CHAR))
+                    {
+                        return LexType.REAL;
+                    }
+                    else
+                    {
+                        if (Analizer.DIGIT_CHARSET.Contains(entry))
+                        {
+                            return this.GetNextState(StatePos.Q3, this.NextChar());
+                        }
+                    }
+                    break;
+                case StatePos.Q4:
+                    if (entry.Equals(Analizer.NULL_CHAR))
+                    {
+                        return LexType.IDENTIFIER;
+                    }
+                    else
+                    {
+                        if (Analizer.DIGIT_CHARSET.Contains(entry))
+                        {
+                            return this.GetNextState(StatePos.Q4, this.NextChar());
+                        }
+                        else if (Analizer.TEXT_CHARSET.Contains(entry))
+                        {
+                            return this.GetNextState(StatePos.Q4, this.NextChar());
+                        }
+                    }
+                    break;
+            }
             return LexType.UNDEFINED;
         }
 
