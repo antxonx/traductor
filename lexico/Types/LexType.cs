2f814b1743250d37a523f683c93ad8625d58c031
alejandro-antonio <alejandro@pcpartners.com.mx>
2021-02-26 11:04:03 -0600

    Type comments

--- lexico/Types/LexType.cs
+++ lexico/Types/LexType.cs
@@ -1,19 +1,49 @@
 ﻿namespace Lexico
 {
+    /// <summary>
+    /// tipos de símbolos
+    /// </summary>
     public enum LexType
     {
+        /// <summary>
+        /// [0-9]+
+        /// </summary>
         INTEGER,
+        /// <summary>
+        /// [0-9]+.[0-9]+
+        /// </summary>
         REAL,
+        /// <summary>
+        /// [a-zA-Z]([a-zA-Z]|[0-9])*
+        /// </summary>
         IDENTIFIER,
+        /// <summary>
+        /// fallback
+        /// </summary>
         UNDEFINED,
     }
 
+    /// <summary>
+    /// Estados del autoamata 
+    /// </summary>
     internal enum StatePos
     {
+        /// <summary>
+        /// Inicio
+        /// </summary>
         Q0,
-        Q1, // aceptación
+        /// <summary>
+        /// Aceptar INTEGER
+        /// </summary>
+        Q1,
         Q2,
-        Q3, // aceptación
-        Q4  // aceptación
+        /// <summary>
+        /// Aceptar Real
+        /// </summary>
+        Q3,
+        /// <summary>
+        /// Aceptar Identificador
+        /// </summary>
+        Q4
     }
 }
