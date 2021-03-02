8949bbda9301746ed3192aa0a894c6a99b4a27b7
alejandro-antonio <alejandro@pcpartners.com.mx>
2021-03-02 12:38:47 -0600

    copied project

---
+++ CompleteLex/Analizer.cs
@@ -0,0 +1,143 @@
+﻿namespace Lexico
+{
+    internal class Analizer
+    {
+        public static readonly string DIGIT_CHARSET = "0123456789";
+
+        public static readonly string TEXT_CHARSET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public static readonly char NULL_CHAR = (char)0;
+
+        public static readonly char DECIMAL_POINT_CHAR = '.';
+
+        private string symbol;
+
+        private int index;
+
+        public Analizer()
+        {
+            this.index = -1;
+        }
+
+        public Analizer(string symbol)
+        {
+            this.symbol = symbol;
+            this.index = -1;
+        }
+
+        public void SetNewSymbol(string symbol)
+        {
+            this.symbol = symbol;
+            this.index = -1;
+        }
+
+        public char GetActualChar()
+        {
+            int index = (this.index < 0) ? 0 : this.index;
+            return this.symbol[index];
+        }
+
+        public char NextChar()
+        {
+            if (!this.IsDone())
+            {
+                this.index++;
+                return this.GetActualChar();
+            }
+            else
+            {
+                return Analizer.NULL_CHAR;
+            }
+        }
+
+        public bool IsDone()
+        {
+            return this.index >= (this.symbol.Length - 1);
+        }
+
+        public string GetSymbol()
+        {
+            return this.symbol;
+        }
+
+        public LexType GetLexType()
+        {
+            return this.GetNextState(StatePos.Q0, this.NextChar());
+        }
+
+        public LexType GetNextState(in StatePos state, in char entry)
+        {
+            switch (state)
+            {
+                case StatePos.Q0:
+                    if (Analizer.DIGIT_CHARSET.Contains(entry))
+                    {
+                        return this.GetNextState(StatePos.Q1, this.NextChar());
+                    }
+                    else if (Analizer.TEXT_CHARSET.Contains(entry))
+                    {
+                        return this.GetNextState(StatePos.Q4, this.NextChar());
+                    }
+                    break;
+
+                case StatePos.Q1:
+                    if (entry.Equals(Analizer.NULL_CHAR))
+                    {
+                        return LexType.INTEGER;
+                    }
+                    else
+                    {
+                        if (Analizer.DIGIT_CHARSET.Contains(entry))
+                        {
+                            return this.GetNextState(StatePos.Q1, this.NextChar());
+                        }
+                        else if (entry.Equals('.'))
+                        {
+                            return this.GetNextState(StatePos.Q2, this.NextChar());
+                        }
+                    }
+                    break;
+
+                case StatePos.Q2:
+                    if (Analizer.DIGIT_CHARSET.Contains(entry))
+                    {
+                        return this.GetNextState(StatePos.Q3, this.NextChar());
+                    }
+                    break;
+
+                case StatePos.Q3:
+                    if (entry.Equals(Analizer.NULL_CHAR))
+                    {
+                        return LexType.REAL;
+                    }
+                    else
+                    {
+                        if (Analizer.DIGIT_CHARSET.Contains(entry))
+                        {
+                            return this.GetNextState(StatePos.Q3, this.NextChar());
+                        }
+                    }
+                    break;
+
+                case StatePos.Q4:
+                    if (entry.Equals(Analizer.NULL_CHAR))
+                    {
+                        return LexType.IDENTIFIER;
+                    }
+                    else
+                    {
+                        if (Analizer.DIGIT_CHARSET.Contains(entry))
+                        {
+                            return this.GetNextState(StatePos.Q4, this.NextChar());
+                        }
+                        else if (Analizer.TEXT_CHARSET.Contains(entry))
+                        {
+                            return this.GetNextState(StatePos.Q4, this.NextChar());
+                        }
+                    }
+                    break;
+            }
+            return LexType.UNDEFINED;
+        }
+    }
+}
---
+++ CompleteLex/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Lexico
+{
+    internal class Program
+    {
+        public const int LEFT_COL_SIZE = 30;
+
+        public const int RIGHT_COL_SIZE = 30;
+
+        private static void Main(string[] args)
+        {
+            string entryPath, file;
+            int index;
+            string symbol = "";
+            string lastSymbol = symbol;
+            bool done = false;
+            bool start = false;
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Des especificar el nombre del archivo al ejecutar el programa");
+                return;
+            }
+            entryPath = Environment.CurrentDirectory + "/" + args[0];
+            file = System.IO.File.ReadAllText(entryPath);
+            file = file.Replace("\n", " ").Replace("\r", " ");
+            PrintTableLimit();
+            Console.WriteLine($"| {"Symbol",-LEFT_COL_SIZE} | {"Type",-RIGHT_COL_SIZE}|");
+            Console.Write("|");
+            for (index = 0; index <= LEFT_COL_SIZE + 1; index++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("|");
+            for (index = 0; index <= RIGHT_COL_SIZE; index++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("|");
+            index = 0;
+            while (index < file.Length)
+            {
+                if (file[index] == ' ')
+                {
+                    if (start)
+                    {
+                        done = true;
+                        start = false;
+                    }
+                }
+                else
+                {
+                    if (!start)
+                    {
+                        start = true;
+                    }
+                    symbol += file[index];
+                }
+                if (done)
+                {
+                    AnalizeSymbol(symbol);
+                    lastSymbol = symbol;
+                    done = false;
+                    symbol = "";
+                }
+                index++;
+            }
+
+            if (symbol.Length > 0 && lastSymbol != symbol)
+            {
+                AnalizeSymbol(symbol);
+            }
+            PrintTableLimit();
+            Console.WriteLine("");
+        }
+
+        public static void PrintTableLimit()
+        {
+            int index;
+            for (index = 0; index <= RIGHT_COL_SIZE + LEFT_COL_SIZE + 5; index++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("");
+        }
+
+        public static void AnalizeSymbol(in string symbol)
+        {
+            Analizer analizer = new Analizer();
+            analizer.SetNewSymbol(symbol);
+            Console.WriteLine($"| {analizer.GetSymbol(),-LEFT_COL_SIZE} | {analizer.GetLexType(),-RIGHT_COL_SIZE}|");
+        }
+    }
+}
